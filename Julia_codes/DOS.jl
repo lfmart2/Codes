7a61f1c8255445f160dfa6106fe62ddda7bf035d
lfmart2 <lfmart@emory.edu>
2024-04-10 11:25:32 -0400

    Refactor file paths and remove unused files

--- Julia_codes/DOS.jl
+++ Julia_codes/DOS.jl
@@ -5,12 +5,22 @@ using Plots.PlotMeasures
 
 
 
-# Files to lead the QE output data
-const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/W.16.Bulk/nscf.out"
+## Files to lead the QE output data
+# const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/W.16.Bulk/nscf.out"
+# const FILE_DOS = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/NbP_pdos.dat.pdos_tot"
+# const FILE_SYM = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/Local/Bulk/NbP_band.labelinfo.dat"
+# const FILE_BAND = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/Local/Bulk/bands.scf.dat.gnu"
+# const FILE_BAND1 = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/W.16.Bulk/NbP_band.dat"
+
+const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/NbP.supercell2x2.scf.out"
 const FILE_DOS = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/NbP_pdos.dat.pdos_tot"
-const FILE_SYM = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/Local/Bulk/NbP_band.labelinfo.dat"
-const FILE_BAND = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/Local/Bulk/bands.scf.dat.gnu"
-const FILE_BAND1 = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/W.16.Bulk/NbP_band.dat"
+const FILE_BAND = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/BZ4/NbP.supercell2x2.bands1.dat.gnu"
+const FILE_SYM = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/BZ4/pp.supercell2x2.bands1.out"
+
+const FILE_BAND1 = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/Local/Bulk1/bulk.bands.dat.gnu"
+const FILE_SYM1 = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/Local/Bulk1/pp.bands.NbP.out"
+const FILE_FE1 = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/Local/Bulk1/scf.out"
+
 
 
 function openfile(data::String)
@@ -66,13 +76,14 @@ function Band_structure_plot(FILE_PATH_BS::String,FILE_PATH_SYM::String,FILE_PAT
     fermi_energy = extract_fermi_energy(FILE_PATH_FE)
     vc_size = findall(x->x==0.0, data[:,1])
     for i in 1:length(vc_size)-1
-        plot!(p1,data[vc_size[i]:vc_size[i+1]-1,1]./data[end,1],data[vc_size[i]:vc_size[i+1]-1,2],lw=0.75,lc=:red,ls=:dash,label=false,xticks = false)
+        plot!(p1,data[vc_size[i]:vc_size[i+1]-1,1]./data[end,1],data[vc_size[i]:vc_size[i+1]-1,2],lw=1.75,lc=:red,ls=:dot,label=false,xticks = false)
     end
-    plot!(p1,data[vc_size[end]:end-1,1]./data[end,1],data[vc_size[end]:end-1,2],lw=0.75,lc=:red,ls=:dash,label="SCF",xticks = false)
+    plot!(p1,data[vc_size[end]:end-1,1]./data[end,1],data[vc_size[end]:end-1,2],lw=0.75,lc=:red,ls=:dot,label="SCF",xticks = false)
     hline!(p1,[fermi_energy],lw=0.75,lc=:black,ls=:dashdot,label=false)
-    vline!(p1,readdlm(FILE_PATH_SYM)[:,3]./readdlm(FILE_PATH_SYM)[end,3], lw=1,lc=:black,ls=:solid,label=false)
+    vline!(p1,sym_points(FILE_PATH_SYM)./sym_points(FILE_PATH_SYM)[end], lw=1,lc=:black,ls=:solid,label=false)
     plot!(p1,xlims=(0,1),grid=false, ylabel="Energy (eV)")
-    plot!(p1,ylim=(8,22), xticks = (readdlm(FILE_PATH_SYM)[:,3]./readdlm(FILE_PATH_SYM)[end,3],["\$\\Gamma \$","\$X \$","\$ M \$","\$ Y \$","\$ K \$","\$ F \$","\$ \\Gamma \$"]))
+    # plot!(p1,ylim=(8,22), xticks = (readdlm(FILE_PATH_SYM)[:,3]./readdlm(FILE_PATH_SYM)[end,3],["\$\\Gamma \$","\$X \$","\$ M \$","\$ Y \$","\$ K \$","\$ F \$","\$ \\Gamma \$"]))
+    plot!(p1,ylim=(1.5,2.5), xticks = (sym_points(FILE_PATH_SYM)./sym_points(FILE_PATH_SYM)[end],["\$\\Gamma \$","\$ Y \$","\$ R_2 \$", "\$ \\Gamma \$"]))
     annotate!(p1,1.5,fermi_energy+0.2, Plots.text("Fermi energy", 8))
     display(p1)
 end
--- Julia_codes/PDOS.jl
+++ Julia_codes/PDOS.jl
@@ -2,7 +2,7 @@ using Plots
 using DelimitedFiles
 using CSV
 
-
+const FOLDER = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/pdos/supercell/"
 
 function openfile(data::String)
     datafile = readdlm(data)
@@ -11,7 +11,7 @@ function openfile(data::String)
     return energy, pdos
 end
 
-const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/Local/Bulk/scf.out"
+const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/pdos/supercell/NbP.supercell2x2.nscf.out"
 
 function openfile1(data::String)
     open(data, "r") do file
@@ -29,33 +29,51 @@ function extract_fermi_energy(FILE_PATH::String)::Float64
     end
 end
 
-const files = filter(x->occursin("NbP_pdos.dat.pdos_atm",x), readdir("/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/"))
+# const files = filter(x->occursin("NbP\\.supercell2x2\\.pdos\\.dat\\.pdos_atm#\\d(Nb)_wfc#.(d)",x), readdir("/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/pdos/supercell/"))
+const files = cat(filter(x->occursin(r"NbP\.supercell2x2\.pdos\.dat\.pdos_atm#.\(Nb\)_wfc#.\(d\)",x), readdir(FOLDER)),
+filter(x->occursin(r"NbP\.supercell2x2\.pdos\.dat\.pdos_atm#.\(P\)_wfc#.\(p\)",x), readdir(FOLDER)),
+filter(x->occursin(r"NbP\.supercell2x2\.pdos\.dat\.pdos_atm#\d{2}\(P\)_wfc#.\(p\)",x), readdir(FOLDER)),dims=1)
 
 
-function plot_pdos(Folder_PATH::Vector{String},M::Vector{Int64})
+function plot_pdos(FOLDER_FILES::String,FILES::Vector{String},M::Vector{Int64})
     p = plot()
-    clrs = [:blue, :red, :green, :blue];
+    clrs = [:blue, :red, :green, :purple, :orange];
     linestyle = [:solid, :dash, :dashdot, :dot]
-    for i in Folder_PATH
-        E,  PDOS = openfile("/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/"*i)
+    for i in FILES[[1,6,9,13]]
+        E,  PDOS = openfile(FOLDER_FILES*i)
         region_condition = zeros(length(E))
-        region_condition[E .> extract_fermi_energy(FILE_FE)] = PDOS[E .> extract_fermi_energy(FILE_FE)]
-        
-        plot!(p,E, PDOS,
-        lw=1.5, label=i[25:end-10]*" "*i[end-3:end], 
-        ls = linestyle[parse(Int, string(i[23]))],
-        lc=clrs[parse(Int, string(i[end-3]))])
+        region_condition[E .< extract_fermi_energy(FILE_FE)] = PDOS[E .< extract_fermi_energy(FILE_FE)]
 
         plot!(p,E, PDOS,
-        fill=(0, region_condition,clrs[parse(Int, string(i[end-3]))], 0.25),
-        lw=1.5, label=false, 
-        ls = linestyle[parse(Int, string(i[23]))],
-        lc=clrs[parse(Int, string(i[end-3]))])
+        lw=1.5, label=i[36:end-9]*"-"*i[end-3:end],
+        lc=clrs[findall(x->x==i,FILES[[1,6,9,13]])],
+        ls=linestyle[findall(x->x==i,FILES[[1,6,9,13]])])
+        # plot!(p,E, PDOS,
+        # lw=1.5, label=i[36:end-9]*"-"*i[end-3:end], 
+        # ls = linestyle[parse(Int, string(i[end-3]))],
+        # lc=clrs[parse(Int, string(i[end-3]))])
+
+        plot!(p,E, region_condition,
+        fill=(0, region_condition,clrs[findall(x->x==i,FILES[[1,6,9,13]])], 0.15),
+        label=false,lw=0)
+
+        # plot!(p,E, region_condition,
+        # fill=(0, region_condition,clrs[parse(Int, string(i[end-3]))], 0.15),
+        # lw=0, label=false,
+        # ls = linestyle[parse(Int, string(i[36]))])
     end
-    Enn,  PPDOS = openfile("/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/NbP_pdos.dat.pdos_tot")
+    Enn,  PPDOS = openfile(FOLDER_FILES*filter(x->occursin(r".pdos_tot",x), readdir(FOLDER_FILES))[1])
     plot!(p,extract_fermi_energy(FILE_FE)*ones(2), [0, findmax(PPDOS)[1]],lw=0.75,lc=:black,ls=:dashdot,label=false)
-    plot!(p,grid=false, yticks=false, xlabel="Energy (eV)", ylabel="DOS (states/eV)", xlims=(M[1],M[2]), ylims=(0, findmax(PPDOS)[1]-10), legend=:topleft)
-    plot!(p,Enn, PPDOS, lw=2,label="Total DOS", lc=:black)
-    annotate!(p,extract_fermi_energy(FILE_FE)-0.2,4, Plots.text("Fermi energy", 12, :dark, rotation = 90 ))
+    plot!(p,extract_fermi_energy(FILE_FE)*ones(2), [0, 2],lw=0.75,lc=:black,ls=:dashdot,label=false)
+    plot!(p,grid=false,
+    yticks=false, xlabel="Energy (eV)",
+    ylabel="DOS (states/eV)", xlims=(M[1],M[2]),
+    ylims=(0, 2), legend=:topleft)
+    #ylims=(0, findmax(PPDOS)[1]-10), legend=:topleft)
+    annotate!(p,extract_fermi_energy(FILE_FE)-0.1,1.5,
+    Plots.text("Fermi energy", 12, :dark, rotation = 90 ) )
+    plot!(twinx(),Enn, PPDOS, lw=2,label="Total DOS", lc=:black,
+    xlims=(M[1],M[2]), ylims=(0, 50),ylabel="Total DOS (states/eV)")
+
     return p
 end
