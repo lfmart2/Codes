3dfcf47335f668064c1fab6164ff269692206a1e
lfmart2 <lfmart@emory.edu>
2024-06-03 10:32:39 -0400

    chore: Refactor file paths and update Band_structure_plot function in DOS_m.jl

--- Julia_codes/DOS_s.jl
+++ Julia_codes/DOS_s.jl
@@ -12,7 +12,7 @@ using Plots.PlotMeasures
 # const FILE_BAND = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/Local/Bulk/bands.scf.dat.gnu"
 # const FILE_BAND1 = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/W.16.Bulk/NbP_band.dat"
 
-const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/NbP.supercell2x2.scf.out"
+const FILE_FE = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/scf.out"
 const FILE_DOS = raw"/home/r_floren/BandStructure/QE/NbP/github/Outputs/fire_cluster/NbP_spin/NbP_pdos.dat.pdos_tot"
 const FILE_BAND = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/BZ4/NbP.supercell2x2.bands1.dat.gnu"
 const FILE_SYM = raw"/home/r_floren/BandStructure/QE/NbP/Main_folder/supercell/BZ4/pp.supercell2x2.bands1.out"
@@ -42,14 +42,20 @@ end
 const FERMI_ENERGY = extract_fermi_energy(FILE_FE)
 
 function plot_dos(FILE_PATH_DOS::String, FILE_PATH_FE::String)
+    # Parameters for the plot
+    LINE_WIDTH = 0.75
+    LINE_COLOR = [:red, :black]
+    FILL_ALPHA = 0.5
+    ANNOTATE_POSITION = -0.2
+    ANNOTATE_SIZE = 12
     fermi_energy = extract_fermi_energy(FILE_PATH_FE)
     energy_data = readdlm(FILE_PATH_DOS)
     p = plot(leftmargin = 5mm)
     plot!(p, energy_data[2:end,1], energy_data[2:end,2], lw=LINE_WIDTH, lc=LINE_COLOR[1], label=false)
     plot!(p, energy_data[2:findmin(abs.(energy_data[2:end,1] .- fermi_energy))[2],1], energy_data[2:findmin(abs.(energy_data[2:end,1] .- fermi_energy))[2],2], fill = (FILL_ALPHA, 0, LINE_COLOR[1]), lw=LINE_WIDTH, lc=LINE_COLOR[1], label=false)
     plot!(p, grid=false, yticks=false, xlabel="Energy (eV)", ylabel="DOS (states/eV)", xlims=(energy_data[2,1], energy_data[end,1]), ylims=(energy_data[2,2], findmax(energy_data[2:end,2])[1]))
-    plot!(p, fermi_energy*ones(2), [0, findmax(energy_data[2:end,2])[1]], lw=LINE_WIDTH, lc=LINE_COLOR[2], ls=:dashdot, label=false)
-    plot!(p, xlims=(8, 19),ylims=(0, 7))
+    plot!(p, fermi_energy*ones(2), [0, findmax(energy_data[2:end,2])[1]], lw=0.75, lc=LINE_COLOR[2], ls=:dashdot, label=false)
+    plot!(p, xlims=(fermi_energy-2, fermi_energy+2),ylims=(0, 7))
     annotate!(p, fermi_energy + ANNOTATE_POSITION, 5, Plots.text("Fermi energy", ANNOTATE_SIZE, :dark, rotation = 90 ))
     display(p)
 end
